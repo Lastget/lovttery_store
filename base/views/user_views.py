22fa0e0436ca0d9190415c37902f8dd484247217
Richard <getrt1991@gmail.com>
2023-02-17 22:50:30 +0800

    first commit

---
+++ base/views/user_views.py
@@ -0,0 +1,115 @@
+
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from django.contrib.auth.models import User 
+
+from base.serializer import ProductSerializer, serializers, UserSerializer, UserSerializerWithToken
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status 
+
+# Serializers in Django REST Framework are responsible for converting 
+# objects into data types understandable by javascript and front-end frameworks.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data 
+        for k, v in serializer.items():
+            data[k] = v 
+
+        return data 
+ 
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['POST'])
+def registerUser(request):
+    # Django REST reques.data 
+    data = request.data
+
+    try:
+        # send the request data object from frontend.  
+        user = User.objects.create(
+            first_name = data['name'],
+            username = data['email'],
+            email =  data['email'],
+            password = make_password(data['password']) 
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except: 
+        message = {'detail': 'User with this email existed'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializers = UserSerializerWithToken(user, many=False)
+
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != '': 
+        user.passwword = make_password(data['password'])
+
+    user.save()
+
+    return Response(serializers.data )
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
+    serializers = UserSerializer(user, many=False)
+    return Response(serializers.data )
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):  
+    users = User.objects.all()
+    serializers = UserSerializer(users, many=True)
+    return Response(serializers.data )
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteUser(request, pk):  
+    usersForDeletion = User.objects.get(id=pk)
+    usersForDeletion.delete()
+    return Response('User was deleted')
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsersById(request,pk):  
+    user = User.objects.get(id =pk)
+    serializers = UserSerializer(user, many=False)
+    return Response(serializers.data )
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateUser(request, pk):
+    user = User.objects.get(id=pk)
+    
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    user.is_staff = data['isAdmin']
+
+    user.save()
+
+    serializers = UserSerializer(user, many=False)
+
+    return Response(serializers.data )
